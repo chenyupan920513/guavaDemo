041722e53917723f928955c2994de6e4e80e2433
pierre.chen <=pierre.chen@shellshellfish.com>
2018-05-19 22:14:51 +0800

    add bloom filter test case

---
+++ src/main/java/com/pierre/test/BloomFilterTest2.java
@@ -0,0 +1,34 @@
+package com.pierre.test;
+
+import com.google.common.hash.BloomFilter;
+import com.google.common.hash.Funnels;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @Author pierre.chen
+ * @Date 18-5-19
+ */
+public class BloomFilterTest2 {
+
+    private static int size = 1000000;
+
+    private static BloomFilter<Integer> bloomFilter = BloomFilter.create(Funnels.integerFunnel(), size);
+
+    public static void main(String[] args) {
+        for (int i = 0; i < size; i++) {
+            bloomFilter.put(i);
+        }
+        long startTime = System.nanoTime(); // 获取开始时间
+
+        //判断这一百万个数中是否包含29999这个数
+        if (bloomFilter.mightContain(29999)) {
+            System.out.println("命中了");
+        }
+        long endTime = System.nanoTime();   // 获取结束时间
+
+        System.out.println("程序运行时间： " + (endTime - startTime) + "纳秒");
+
+    }
+}
